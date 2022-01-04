e9fc261158c81692c07e68470581ccbfbc299971
Ashwinkumar R <ashwin@artifintel.in>
2022-01-04 22:24:47 +0530

    Added map data struture

---
+++ Maps/Maps.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Print("Maps in Go")
+	map_1 := map[int]string{
+		5001: "Ashwin", 5002: "Joseph", 5003: "Kasim",
+	}
+	fmt.Println("The Map elements are:", map_1)
+
+}
